6921cfcdef72dc5af6110320a671e24771aa4fde
tamlynlou <71732692+tamlynlou@users.noreply.github.com>
2021-01-31 12:22:57 -0500

    my first commit
    Done with layout. Testing our AJAX feature

---
+++ script.js
@@ -0,0 +1,20 @@
+function start(){
+
+}
+
+$(window).on("load", start);
+
+function loadpage(e){
+    
+    e.preventDefault();
+    $("#menu a.active").removeClass("active");
+    $(this).addClass("active");
+
+    $("#content").load("about.html");
+
+}
+
+$(document).on("click", "#menu a", loadpage);
+
+
+
